6d0b79294d2ab3cbcc00c36d6817a2741cf0f9a3
Gonçalo <gteixei@uva.nl>
2025-03-04 19:56:40 +0100

    Added docker-compose and kubernetes configuration files

--- server.go
+++ server.go
@@ -1,15 +1,29 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+   "fmt"
+   "net/http"
+
+   "github.com/prometheus/client_golang/prometheus"
+   "github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var pingCounter = prometheus.NewCounter(
+   prometheus.CounterOpts{
+       Name: "ping_request_count",
+       Help: "No of request handled by Ping handler",
+   },
 )
 
 func ping(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w,"pong")
+   pingCounter.Inc()
+   fmt.Fprintf(w, "pong")
 }
 
 func main() {
-	http.HandleFunc("/ping",ping)
-	http.ListenAndServe(":8090",nil)
+   prometheus.MustRegister(pingCounter)
+
+   http.HandleFunc("/ping", ping)
+   http.Handle("/metrics", promhttp.Handler())
+   http.ListenAndServe(":8090", nil)
 }
